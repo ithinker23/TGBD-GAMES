3f7d513c23a4207f45e4fb0d13d398b4037b4654
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2018-12-20 00:50:29 -0800

    js and better buttons

---
+++ TGBD-Games/WebContent/index.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+console.log("Ready");
+$(document).ready(function () {
+
+
+$("#2D2PGameBTN").click(function(){
+	
+	window.location.href = "http://localhost:8080/TGBD-Games-2D2P-Game/Pages/2D2P-Game/2D2P-Game.html";
+	console.log("redirecting to 2D2PGame.html");
+})
+	
+	
+
+
+$("#DriftyRacerBTN").click(function(){
+	
+	window.location.href = "http://localhost:8080/TGBD-Games-2D2P-Game/Pages/Driftyracer/DriftyRacer.html";
+	console.log("redirecting to DriftyRacer.html");
+})
+})
