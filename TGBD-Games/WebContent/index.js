359f8c3c2fa108d072e43c2f5d93d3805422905b
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2018-12-20 17:44:45 -0800

    Major Updates To Website
    Added Contact us, About, Game, and improved Home page

--- TGBD-Games/WebContent/index.js
+++ TGBD-Games/WebContent/index.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 $("#2D2PGameBTN").click(function(){
 	
 	window.location.href = "https://www.tgbdgames.tk/Pages/2D2P-Game/2D2P-Game.html";
-	console.log("redirecting to 2D2PGame.html");
+	console.log("redirecting to 2D2PGame");
 })
 	
 	
@@ -17,6 +17,31 @@ $("#2D2PGameBTN").click(function(){
 $("#DriftyRacerBTN").click(function(){
 	
 	window.location.href = "https://www.tgbdgames.tk/Pages/Driftyracer/DriftyRacer.html";
-	console.log("redirecting to DriftyRacer.html");
+	console.log("redirecting to DriftyRacer");
 })
+
+
+$("#HomeBTN").click(function(){
+	
+	window.location.href = "https://www.tgbdgames.tk";
+	console.log("redirecting to HomePage");
 })
+
+$("#GamesBTN").click(function(){
+	
+	window.location.href = "https://www.tgbdgames.tk/Pages/Game-Landing-Page/GamePage.html";
+	console.log("redirecting to Games-Landing-Page");
+})
+
+$("#AboutUsBTN").click(function(){
+	
+	window.location.href = "https://www.tgbdgames.tk/Pages/About-Us-Page/AboutUs.html";
+	console.log("redirecting to About Us Page");
+})
+
+$("#ContactBTN").click(function(){
+	
+	window.location.href = "https://www.tgbdgames.tk/Pages/Contact-Page/ContactUs.html";
+	console.log("redirecting to Contact Form Page");
+})
+});
