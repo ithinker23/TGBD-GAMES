c7141c06ee080495b9a453c6d0d30a1d0a638b29
ithinker23 <44293948+ithinker23@users.noreply.github.com>
2019-01-11 09:50:53 -0800

    Update contact-form-handler.php

--- TGBD-Games/WebContent/Pages/Contact-Page/contact-form-handler.php
+++ TGBD-Games/WebContent/Pages/Contact-Page/contact-form-handler.php
@@ -1,3 +1,27 @@
+
+
+
+
+
+<?php
+$errors = '';
+$myemail = 'yourname@website.com';//<-----Put Your email address here.
+if(empty($_POST['name'])  ||
+   empty($_POST['email']) ||
+   empty($_POST['message']))
+{
+    $errors .= "\n Error: all fields are required";
+}
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+if (!preg_match(
+"/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+$email_address))
+{
+    $errors .= "\n Error: Invalid email address";
+}
+
 if( empty($errors))
 {
 $to = $myemail;
@@ -11,3 +35,11 @@ mail($to,$email_subject,$email_body,$headers);
 //redirect to the 'thank you' page
 header('Location: contact-form-thank-you.html');
 }
+?>
+<script language="JavaScript">
+var frmvalidator  = new Validator("contactform");
+frmvalidator.addValidation("name","req","Please provide your name");
+frmvalidator.addValidation("email","req","Please provide your email");
+frmvalidator.addValidation("email","email",
+  "Please enter a valid email address");
+</script>
